1b5d873ed1408405751a0d4a5beddf64d298d3a4
bielrossi15 <bielrossiborba@gmail.com>
2021-09-15 20:18:56 -0300

    Corrigindo erros do sonar cloud

--- tests/urls_tests.py
+++ tests/urls_tests.py
@@ -1,17 +1,20 @@
 import pytest
 from rest_framework.test import APIClient
+from dotenv import load_dotenv
+import os
 
 
 @pytest.mark.django_db(transaction=False)
 class TestUserEndpoints:
 
     def test_create(self):
+        load_dotenv()
         data = {
             "username": "test",
             "first_name": "test",
             "last_name": "test",
             "cpf": "11111111111",
-            "password": "pswd"
+            "password": os.getenv('POSTGRES_HOST')
         }
 
         api_client = APIClient()
